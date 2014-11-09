70eeb7418e1e58f5e24f64f66544c67be9083c52
Timothy Chong <timothychong@outlook.com>
2014-11-09 10:58:16 -0500

    Interface basically done. Added functions to RTree.java

--- NearestFinder/src/Accessor.java
+++ NearestFinder/src/Accessor.java
@@ -1,4 +1,12 @@
+import java.util.ArrayList;
 
 public interface Accessor {
+	
+	public ArrayList<RTree.Node> getLocationsAtCoord(double lon, double lat, double radius);
 
+	public ArrayList<RTree.Node> getLocationsAtNode(RTree.Node node, double radius);
+	
+	public ArrayList<RTree.Node> getNearestKLocationsAtCoord(double lon, double lat, int k);
+	
+	public ArrayList<RTree.Node> getNearestKLocationsAtNode(RTree.Node node, int k);
 }
--- NearestFinder/src/RTree.java
+++ NearestFinder/src/RTree.java
@@ -1,9 +1,45 @@
+import java.util.ArrayList;
+
+
 
 public class RTree implements Accessor{
 
+	public class Node {
+		
+		double lon;
+		double lat;
+		
+	}
+	
 	public static void main(String [] args){
 		
 		System.out.println("Hello World");
 	
+	}
+
+	@Override
+	public ArrayList<Node> getLocationsAtCoord(double lon, double lat,
+			double radius) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<Node> getLocationsAtNode(Node node, double radius) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<Node> getNearestKLocationsAtCoord(double lon, double lat,
+			int k) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<Node> getNearestKLocationsAtNode(Node node, int k) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
